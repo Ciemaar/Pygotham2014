ce8a5ae8b6adf2c16c894163ab124ea207bae990
Andy Fundinger <Andy@fundinger.name>
2014-08-14 19:25:05 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='PersonalMarketRiskPlatform', version='', packages=['test'], url='', license='',
+      author='Andy Funidinger, Mario Morales', author_email='Andy.Fundinger@riskfocus.com',
+      description='An example market risk platform built for our talk at PyGotham 2014. ',
+      requires=['Quandl>=2.0', 'PyYaml', 'pandas'])
