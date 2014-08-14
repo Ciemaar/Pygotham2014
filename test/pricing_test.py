e3bbcd56a397896e2ce432af7af866f0d48bfcbb
Andy Fundinger <Andy@fundinger.name>
2014-08-14 18:15:43 -0400

    reformatted

--- test/pricing_test.py
+++ test/pricing_test.py
@@ -13,6 +13,7 @@ __author__ = 'andriod'
 
 import unittest
 
+
 class PricingCase(unittest.TestCase):
     def setUp(self):
         self.funds = FundsHolder('funds')
@@ -21,7 +22,7 @@ class PricingCase(unittest.TestCase):
 
     def test_price(self):
         position_info = self.funds.rip.T[1]
-        self.assertAlmostEqual(price_holding(position_info,self.market,self.model,'2014-03-03'),447.2384)
+        self.assertAlmostEqual(price_holding(position_info, self.market, self.model, '2014-03-03'), 447.2384)
 
     def test_price2(self):
         position_info = {'instrument_type': 'stock', 'end_date': None, 'instrument': 'SCI', 'cost': None,
@@ -31,7 +32,9 @@ class PricingCase(unittest.TestCase):
     def test_price_all(self):
         all_prices = sorted(fund.price(self.market, self.model, '2014-05-14') for fund in self.funds)
         self.assertEqual(len(all_prices), 6)
-        self.assertListEqual(all_prices, sorted([4139.9243000000006, 85.053227000000007,  4099.5227000000004,  122.020098,  96.709960000000024,  59459.754800000002]))
+        self.assertListEqual(all_prices, sorted(
+            [4139.9243000000006, 85.053227000000007, 4099.5227000000004, 122.020098, 96.709960000000024,
+             59459.754800000002]))
 
 
 if __name__ == '__main__':
