2490650ffc99703bba17b4e8a5a0504c234e2b69
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-08-14 23:55:24 -0400

    set for proper current 10 funds

--- test/pricing_test.py
+++ test/pricing_test.py
@@ -31,7 +31,7 @@ class PricingCase(unittest.TestCase):
 
     def test_price_all(self):
         all_prices = sorted(fund.price(self.market, self.model, '2014-05-14') for fund in self.funds)
-        self.assertEqual(len(all_prices), 6)
+        self.assertEqual(len(all_prices), 10)
         self.assertListEqual(all_prices, sorted(
             [4139.9243000000006, 85.053227000000007, 4099.5227000000004, 122.020098, 96.709960000000024,
              59459.754800000002]))
