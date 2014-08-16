c856f20dd3f18608a620ae8b8613afed4aa62ce3
Andy Fundinger <Andy.Fundinger@riskfocus.com>
2014-08-15 22:58:39 -0400

    Wrapped into a script for periodic analysis

--- AnalyzePortfolio.py
+++ AnalyzePortfolio.py
@@ -1,4 +1,6 @@
+import logging
 from pandas import DataFrame
+import sys
 
 from data_access import get_file
 from fund import FundsHolder
@@ -9,27 +11,30 @@ from simple_files import FileHolder
 
 __author__ = 'Andy Fundinger - Andy.Fundinger@riskfocus.com'
 
-portfolios = PortfoliosHolder('portfolio')
-funds = FundsHolder('funds')
-model = FileHolder('model')
-results = FileHolder('results')
-market = get_live(get_file("sys"), model)
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
 
-cob_date = '8/1/2014'
+    portfolios = PortfoliosHolder('portfolio')
+    funds = FundsHolder('funds')
+    model = FileHolder('model')
+    results = FileHolder('results')
+    market = get_live(get_file("sys"), model)
 
-risk_by_fund = DataFrame(
-    ((fund.name, fund.price(market, model, cob_date), fund.hVar(market, model, cob_date)) for fund in funds),
-    columns=['fund_name', 'price', 'hVar'])
-risk_by_fund['normalized_hVar'] = risk_by_fund['hVar'] / risk_by_fund['price']
+    cob_date = sys.argv[1]
 
-results['risk_by_fund'] = risk_by_fund
+    risk_by_fund = DataFrame(
+        ((fund.name, fund.price(market, model, cob_date), fund.hVar(market, model, cob_date)) for fund in funds),
+        columns=['fund_name', 'price', 'hVar'])
+    risk_by_fund['normalized_hVar'] = risk_by_fund['hVar'] / risk_by_fund['price']
 
-risk_by_portfolio = DataFrame(
-    ((portfolio.name, portfolio.price(funds, market, model, cob_date), portfolio.hVar(funds, market, model, cob_date))
-     for portfolio in portfolios), columns=['portfolio_name', 'price', 'hVar'])
-risk_by_portfolio['normalized_hVar'] = risk_by_portfolio['hVar'] / risk_by_portfolio['price']
+    results['risk_by_fund'] = risk_by_fund
 
-results['risk_by_portfolio'] = risk_by_portfolio
+    risk_by_portfolio = DataFrame(((portfolio.name, portfolio.price(funds, market, model, cob_date),
+                                    portfolio.hVar(funds, market, model, cob_date)) for portfolio in portfolios),
+                                  columns=['portfolio_name', 'price', 'hVar'])
+    risk_by_portfolio['normalized_hVar'] = risk_by_portfolio['hVar'] / risk_by_portfolio['price']
 
-print risk_by_fund
-print risk_by_portfolio
+    results['risk_by_portfolio'] = risk_by_portfolio
+
+    print risk_by_fund
+    print risk_by_portfolio
